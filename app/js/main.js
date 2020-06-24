37680656a9f96ff31cf81abc12c70b92f42e8bcf
frgwork <frgwork@ya.ru>
2020-06-24 22:52:47 +0300

    followers

--- app/js/main.js
+++ app/js/main.js
@@ -6,10 +6,23 @@ $(function () {
       readOnly: true
    });
 
-   $('.fslaider__box').slick ({
+   $('.fslaider__box').slick({
       nextArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt="1"></button>',
       prevArrow: '<button class="slick-arrow slick-next"><img src="images/prev.svg" alt="2"></button>',
       fade: true,
    });
+
+   $('.followers__products-box').slick({
+      nextArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt="3"></button>',
+      prevArrow: '<button class="slick-arrow slick-next"><img src="images/prev.svg" alt="4"></button>',
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3
+
+   });
+
+
+   var mixer = mixitup('.release__products-box');
 
 });

3147b7a2f0064315c6a779025d25287190ac8eaf
frgwork <frgwork@ya.ru>
2020-06-24 18:03:32 +0300

    products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
 
+   $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+   });
+
+   $('.fslaider__box').slick ({
+      nextArrow: '<button class="slick-arrow slick-prev"><img src="images/next.svg" alt="1"></button>',
+      prevArrow: '<button class="slick-arrow slick-next"><img src="images/prev.svg" alt="2"></button>',
+      fade: true,
+   });
+
 });
